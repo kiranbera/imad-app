83c2884ac1e0359426176862260b5ed1cedc1990
kiranbera <31177649+kiranbera@users.noreply.github.com>
2017-08-22 21:08:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element =document.getElementById('main-text');
 
 element.innerHTML ='New Value';
 
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onClick = function() {
     img.style.marginleft = '100px';
 };
